94fafc2be94856afe5f960e545abe8039589a1cb
matte <matte@DESKTOP-C6A18PK.lan>
2021-04-26 12:10:07 +0200

    Versione in classe

--- src/main/java/it/polito/tdp/metroparis/db/DBConnect.java
+++ src/main/java/it/polito/tdp/metroparis/db/DBConnect.java
@@ -24,7 +24,7 @@ public class DBConnect {
 
 			ds.setJdbcUrl(jdbcURL);
 			ds.setUsername("root");
-			ds.setPassword("root");
+			ds.setPassword("hitmon5");
 
 			// configurazione MySQL
 			ds.addDataSourceProperty("cachePrepStmts", "true");
